a1b09f314bbee1e9037dc98abe44b5c9e1abfbbf
Stefan Bischof <stbischof@bipolis.org>
2022-05-13 18:57:44 +0200

    initial

---
+++ run.sh
@@ -0,0 +1,2 @@
+mvn clean verify -f org.osgi.compatibility.jdbc/org.osgi.compatibility.jdbc.osgi.impl/pom.xml
+mvn clean verify -f org.osgi.compatibility.jdbc/org.osgi.compatibility.jdbc.mssqlserver/pom.xml
